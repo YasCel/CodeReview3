c2b402f59acad96e0af6d9e236e379fd17a5563d
Yasemin Celik <yasemincelik_7@hotmail.com>
2015-10-09 23:21:22 -0700

    Add variables for js file

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $(#blanks form).submit(function(event){
+    var name1input = $("input#name1").val();
+    var favoriteFoodinput = $("input#favoriteFood").val();
+    var favoriteBookinput = $("input#favoriteBook").val();
+    var favoriteMovieinput = $("input#favoriteMovie").val();
+    var favoriteCity = $("input#favoriteCity").val();
+  });
+});
