e736a755afcde1a0bc57380c6f40cde1a993551d
Yasemin Celik <yasemincelik_7@hotmail.com>
2015-10-09 23:26:43 -0700

    Add event handlers to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,15 @@
 $(document).ready(function(){
   $(#blanks form).submit(function(event){
-    var name1input = $("input#name1").val();
-    var favoriteFoodinput = $("input#favoriteFood").val();
-    var favoriteBookinput = $("input#favoriteBook").val();
-    var favoriteMovieinput = $("input#favoriteMovie").val();
+    var name1Input = $("input#name1").val();
+    var favoriteFoodInput = $("input#favoriteFood").val();
+    var favoriteBookInput = $("input#favoriteBook").val();
+    var favoriteMovieInput = $("input#favoriteMovie").val();
     var favoriteCity = $("input#favoriteCity").val();
+
+    $(".name1").text(name1Input);
+    $(".favoriteFood").text(favoriteFoodInput);
+    $(".favoriteBook").text(favoriteBookInput);
+    $(".favoriteMovie").text(favoriteMovieInput);
+    $(".favoriteCity").text(favoriteCity);
   });
 });
